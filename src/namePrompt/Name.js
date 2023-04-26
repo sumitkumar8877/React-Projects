b3407e386dfba1c2aa62d5532b25be36a1ecf81f
sumitkumar8877 <sumitkrsshsmznr@gmail.com>
2023-04-26 17:35:58 +0530

    i have commited name Chane Assignment

---
+++ src/namePrompt/Name.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Name.css'
+
+function Name() {
+  return (
+    <>
+        <form>
+            <div className='submittext'>
+            <input type='text'></input>
+            <button>Submit</button>
+            </div>
+        </form>
+    </>
+  )
+}
+
+export default Name
