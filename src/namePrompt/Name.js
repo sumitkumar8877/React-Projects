72e1dd75bfe4820018fe7645f5a7538a47b05ea0
sumitkumar8877 <sumitkrsshsmznr@gmail.com>
2023-04-26 18:53:10 +0530

    today 2nd Assignment Commited

--- src/namePrompt/Name.js
+++ src/namePrompt/Name.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
 import './Name.css'
 
 function Name() {
+  const [name, setName] = useState("");
+  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setName(event.target.elements.name.value);
+    event.target.elements.name.value = "";
+  };
+
   return (
-    <>
-        <form>
-            <div className='submittext'>
-            <input type='text'></input>
-            <button>Submit</button>
-            </div>
-        </form>
-    </>
-  )
+    <div>
+    <div className="main">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p className="fname">Name:</p>
+          <input className="nameText" type="text" name="name" placeholder="Enter Your Name"/>
+        </label>
+        <button className="nameSubmit" type="submit">Submit</button>
+      </form>
+      {name && <h1 className="displayText">Hii My Name is {name}</h1>}
+      </div>
+    </div>
+  );
 }
 
-export default Name
+export default Name;
